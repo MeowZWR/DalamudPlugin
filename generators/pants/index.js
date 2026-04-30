71ba5155528613e05c41b5f334ad151fd496e28b
MeowZ#WR <antabakala@hotmail.com>
2026-04-30 09:00:38 +0800

    Bump targetApiLevel to 15 in generators

--- generators/pants/index.js
+++ generators/pants/index.js
@@ -4,7 +4,7 @@ const extraTag = "Pants";
 const reposMeta = JSON.parse(fs.readFileSync("./meta.json", "utf8"));
 const final = [];
 
-const targetApiLevel = 14;
+const targetApiLevel = 15;
 const mirrorPrefix = "https://meowrs.com/";
 
 // Get the total download count for a GitHub repo's releases
